aa0b0057870413da1749508b7e977f08263b8b4f
Mohan Sandesh <588700+splendidexponent@users.noreply.github.com>
2023-09-04 09:52:04 +0530

    added syncviews.js

---
+++ sync-views.js
@@ -0,0 +1,50 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/static
+class SyncViews{
+  static debug = false;
+  static view_classes = {};
+  static view_instances = [];
+
+  static addViewClass(viewClass){
+    this.view_classes[viewClass.name] = viewClass;
+
+    if(this.debug){
+      console.log('SyncViews:', 'Added View Class', viewClass.name);
+    }
+  }
+
+  static sync(){
+    // Select all uninitialized views
+    const els = document.querySelectorAll('[data-view]:not([data-view-initialized])');
+
+    // Instantiate
+    els.forEach((el)=>{
+      if(!this.view_classes[el.dataset.view]) return;
+
+      el.dataset.viewInitialized = 'true';
+      this.view_instances.push({
+        el: el,
+        instance: new this.view_classes[el.dataset.view](el)
+      });
+
+      if(this.debug){
+        console.log('SyncViews:', 'Instantiated', el.dataset.view, 'for el:', el);
+      }
+    });
+
+    // Remove instances that are not in dom
+    this.view_instances = this.view_instances.filter((view_instance)=>{
+      if(document.body.contains(view_instance.el) == false){
+        if(this.debug){
+          console.log('SyncViews:', 'Removed', view_instance.instance.constructor.name , 'instance for el:', view_instance.el);
+        }
+
+        view_instance.instance.undelegateEvents();
+        delete view_instance.instance;
+        delete view_instance.el;
+        return false;
+      }
+
+      return true;
+    });
+  }
+}
